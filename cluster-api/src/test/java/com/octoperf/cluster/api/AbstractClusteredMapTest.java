356572270a2a5196178f08c2b217fc95c17d8e80
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2020-08-16 16:27:24 +0200

    tests fixed

--- cluster-api/src/test/java/com/octoperf/cluster/api/AbstractClusteredMapTest.java
+++ cluster-api/src/test/java/com/octoperf/cluster/api/AbstractClusteredMapTest.java
@@ -1,65 +1,65 @@
-//package com.octoperf.cluster.api;
-//
-//import com.google.common.collect.ImmutableMap;
-//import com.google.common.testing.NullPointerTester;
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import java.util.Map;
-//import java.util.Optional;
-//
-//import static com.google.common.testing.NullPointerTester.Visibility.PACKAGE;
-//import static org.junit.Assert.assertEquals;
-//
-//public abstract class AbstractClusteredMapTest<V> {
-//
-//  private ClusteredMap<String, V> service;
-//
-//  private V value;
-//
-//  @Before
-//  public void before() {
-//    service = newService();
-//    value = newValue();
-//  }
-//
-//  @Test
-//  public void shouldPutGet() {
-//    service.put("key", value);
-//    assertEquals(value, service.get("key").orElse(null));
-//  }
-//
-//  @Test
-//  public void shouldPutRemove() {
-//    service.put("key", value);
-//    service.remove("key");
-//    assertEquals(Optional.empty(), service.get("key"));
-//  }
-//
-//  @Test
-//  public void shouldCopyOf() {
-//    service.put("key", value);
-//    final Map<String, V> copyOf = service.copyOf();
-//    assertEquals(ImmutableMap.of("key", value), copyOf);
-//  }
-//
-//  @Test
-//  public void shouldDestroy() {
-//    service.destroy();
-//  }
-//
-//  @Test
-//  public void shouldPassNPETester() {
-//    new NullPointerTester().testConstructors(service.getClass(), PACKAGE);
-//  }
-//
-//  @After
-//  public void after() {
-//    service.destroy();
-//  }
-//
-//  protected abstract ClusteredMap<String, V> newService();
-//
-//  protected abstract V newValue();
-//}
+package com.octoperf.cluster.api;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.testing.NullPointerTester;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.Optional;
+
+import static com.google.common.testing.NullPointerTester.Visibility.PACKAGE;
+import static org.junit.Assert.assertEquals;
+
+public abstract class AbstractClusteredMapTest<V> {
+
+  private ClusteredMap<String, V> service;
+
+  private V value;
+
+  @Before
+  public void before() {
+    service = newService();
+    value = newValue();
+  }
+
+  @Test
+  public void shouldPutGet() {
+    service.put("key", value);
+    assertEquals(value, service.get("key").orElse(null));
+  }
+
+  @Test
+  public void shouldPutRemove() {
+    service.put("key", value);
+    service.remove("key");
+    assertEquals(Optional.empty(), service.get("key"));
+  }
+
+  @Test
+  public void shouldCopyOf() {
+    service.put("key", value);
+    final Map<String, V> copyOf = service.copyOf();
+    assertEquals(ImmutableMap.of("key", value), copyOf);
+  }
+
+  @Test
+  public void shouldDestroy() {
+    service.destroy();
+  }
+
+  @Test
+  public void shouldPassNPETester() {
+    new NullPointerTester().testConstructors(service.getClass(), PACKAGE);
+  }
+
+  @After
+  public void after() {
+    service.destroy();
+  }
+
+  protected abstract ClusteredMap<String, V> newService();
+
+  protected abstract V newValue();
+}
--- cluster-api/src/test/java/com/octoperf/cluster/api/ForwardingClusteredQueueTest.java
+++ cluster-api/src/test/java/com/octoperf/cluster/api/ForwardingClusteredQueueTest.java
@@ -1,9 +1,9 @@
-//package com.octoperf.cluster.api;
-//
-//public class ForwardingClusteredQueueTest extends AbstractClusteredQueueTest {
-//
-//  @Override
-//  protected ClusteredQueue<String> newService() {
-//    return new TestForwardingClusteredQueue<>(() -> {});
-//  }
-//}
+package com.octoperf.cluster.api;
+
+public class ForwardingClusteredQueueTest extends AbstractClusteredQueueTest {
+
+  @Override
+  protected ClusteredQueue<String> newService() {
+    return new TestForwardingClusteredQueue<>(() -> {});
+  }
+}
